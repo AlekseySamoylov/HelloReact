3f673bbae874ac8e52eea1a522d56ea255474b29
AlekseySamoylov <alekseysamoylov89@gmail.com>
2017-10-27 08:15:59 +0300

    Second lesson

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,44 @@
+var Greeter = React.createClass({
+    getDefaultProps: function() {
+        return {
+            name: 'React'
+        };
+    },
+    render: function() {
+        var name = this.props.name;
+        return (
+            <div>
+                <h1>Hello {name}!</h1>
+            </div>
+        )
+    }
+});
+
+let name = 'Aleksey';
+
 ReactDOM.render(
-    <h1>Hello React</h1>,
+    <Greeter name={name}/>,
     document.getElementById("app")
+);
+
+var Paragraph = React.createClass({
+    getDefaultProps: function() {
+        return {
+            paragraph: 'Default message'
+        };
+    },
+    render: function() {
+        var paragraph = this.props.paragraph;
+        return (
+            <div>
+                <p>{paragraph}</p>
+            </div>
+        )
+    }
+});
+
+let paragraph = 'Hello new paragraph from prop';
+ReactDOM.render(
+    <Paragraph paragraph = {paragraph}/>,
+    document.getElementById("paragraph")
 );
